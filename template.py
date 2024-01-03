f541c866c5595b675f1f82f778b51df945dddb86
Suman <sumanryu@gmail.com>
2024-01-04 01:59:47 +0530

    Requirements installation

--- template.py
+++ template.py
@@ -2,14 +2,15 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s], %(messages)s:')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
 
 project_name = "textSummarizer"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
-    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/conponents/__init__.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/utils/common.py",
     f"src/{project_name}/logging/__init__.py",
@@ -26,7 +27,7 @@ list_of_files = [
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
-    
+
 ]
 
 
@@ -34,14 +35,17 @@ for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
 
-    if filedir !="":
+    if filedir != "":
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory:{filedir} for the file {filename}")
-    
-    if (not os.path.exists(filepath)) or (os.path.getsize(filename) == 0):
-        with open(filepath, 'w') as f:
-            pass
-            logging.info(f"creating empty file:{filepath}")
 
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    
     else:
-        logging.info(f"{filename} is already existing")
+        logging.info(f"{filename} is already exists")
